d6293e123ba23743c5f3914fe0073252bb5c9b5a
Priyadarshini K B <priyadarshinii.kb.4@gmail.com>
2025-07-31 01:18:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
+
+// Azure will set process.env.PORT (usually to 80), use that or fallback to 3000 locally
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => res.send('Hello from Azure!'));
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
